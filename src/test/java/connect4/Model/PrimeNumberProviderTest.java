092985cb3fe381164fc0ddf49f27879a09356b88
Dane Wicki <wicki.dane@gmail.com>
2019-03-22 14:02:01 +0100

    Add a first Testclass

---
+++ src/test/java/connect4/Model/PrimeNumberProviderTest.java
@@ -0,0 +1,13 @@
+package connect4.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeNumberProviderTest {
+
+    @Test
+    void nextPrimeNumber() {
+        assertTrue(true);
+    }
+}
